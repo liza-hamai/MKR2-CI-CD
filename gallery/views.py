0d56b735403e83382eec9c6cb38b4c5e3c72349c
liza-hamai <elizaveta.hamai@knu.ua>
2025-05-23 11:17:02 +0300

    added image_detail

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from .models import Category, Image
 def gallery_view(request):
     categories = Category.objects.prefetch_related('image_set').all()
     return render(request, 'gallery.html', {'categories': categories})
+
+def image_detail(request, image_id):
+    image = get_object_or_404(Image, id=image_id)
+    return render(request, 'image_detail.html', {'image': image})
