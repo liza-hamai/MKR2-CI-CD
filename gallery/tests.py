acd03a07aa4d5f9d7447ccfc76b33ffcbc059a60
liza-hamai <elizaveta.hamai@knu.ua>
2025-05-23 11:25:25 +0300

    added GalleryViewsTestCase

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Category, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
 
-# Create your tests here.
+
+class GalleryViewsTestCase(TestCase):
+    def setUp(self):
+        # Тестова категорія
+        self.category = Category.objects.create(name="Nature")
+
+        # Фейкове зображення
+        self.image = Image.objects.create(
+            category=self.category,
+            title="Mountain",
+            image=SimpleUploadedFile(name='test_image.jpg', content=b'', content_type='image/jpeg')
+        )
