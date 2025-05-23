1fe0e536b99c753fd4216f4bbfbf0482fbc161de
liza-hamai <elizaveta.hamai@knu.ua>
2025-05-23 11:16:46 +0300

    added gallery_view

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Category, Image
+
+def gallery_view(request):
+    categories = Category.objects.prefetch_related('image_set').all()
+    return render(request, 'gallery.html', {'categories': categories})
